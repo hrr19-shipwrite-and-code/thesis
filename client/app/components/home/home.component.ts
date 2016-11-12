557528b8a7e8ebd3ee5830dd9bcaeca5ae7f2afc
Jacob Goddard <jrglivin@gmail.com>
2016-11-12 15:00:14 -0800

    hover directive created and implemented on home

--- client/app/components/home/home.component.ts
+++ client/app/components/home/home.component.ts
@@ -1,19 +1,28 @@
 import {Component} from 'angular2/core';
 import {HomeService} from './home.services.js';
+import {HoverDirective} from '../../directives/thumbnail-hover.directive.js';
 
 @Component({
   selector: 'home',
   templateUrl: './client/app/components/home/home.html',
-  providers: [HomeService]
+  providers: [HomeService],
+  directives: [HoverDirective]
 })
 
 export class HomeComponent {
   projects;
+  hoverShow = true;
   constructor(homeService: HomeService){
     this.projects = homeService.getProjects();
   }
 
-  over(){
-    console.log("Mouseover called");
-  }
+  // hover(){
+  //   console.log("Mouseover called");
+  //   this.hoverShow = true;
+  // }
+
+  // hoverStop(){
+  //   console.log("Mouseover called");
+  //   this.hoverShow = false;
+  // }
 }
