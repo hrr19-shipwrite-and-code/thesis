f9d499481f077d0b955294169b8723c02f3b3f7a
Cameron Baughn <cam.baughn@gmail.com>
2016-11-12 13:02:50 -0800

    Added FontAwesome CDN

---
+++ client/app/components/profile/profile.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'profile',
+  templateUrl: './client/app/components/profile/profile.html'
+})
+
+export class ProfileComponent { }
