5e3751e951fe3d5e0eb71454045a813e94104431
Jacob Goddard <jrglivin@gmail.com>
2016-11-14 22:39:50 -0800

    starting switch to new router

---
+++ client/app/components/auth/auth.service.js
@@ -0,0 +1,25 @@
+// import {Injectable} from 'angular2/core';
+// import {tokenNotExpired} from 'angular2-jwt';
+// declare var Auth0Lock: any;
+// @Injectable()
+// export class AuthService {
+//  lock = new Auth0Lock('wtgfH9yCpAyHiTrupNH3xXsMPh0WfxYR', 'nanciee.auth0.com');
+//  login() {
+//    this.lock.show((error: string, profile: Object, id_token: string) => {
+//      if (error) {
+//        console.log(error);
+//      }
+//      console.log(id_token)
+//      localStorage.setItem('profile', JSON.stringify(profile));
+//      localStorage.setItem('id_token', id_token);
+//    });
+//  }
+//  logout() {
+//    localStorage.removeItem('profile');
+//    localStorage.removeItem('id_token');
+//  }
+//  public authenticated() {
+//     return tokenNotExpired();
+//   };
+// } 
+//# sourceMappingURL=auth.service.js.map
