a4c62dc4be8495b3fec0a7b8b0ffe02748bce4ad
Hughes89 <mik3h89@gmail.com>
2016-11-29 17:31:39 -0500

    Feat: create footer component/html/css

---
+++ client/app/components/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'footer',
+  templateUrl: './client/app/components/footer/footer.html',
+  styleUrls: ['./client/app/components/footer/footer.css'],
+})
+
+export class FooterComponent {
+}
