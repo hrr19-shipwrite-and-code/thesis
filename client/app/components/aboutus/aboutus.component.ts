807f3adaa942d5ce7c71a49e3fecc488850d52ac
Hughes89 <mik3h89@gmail.com>
2016-11-29 17:29:29 -0500

    Feat: create aboutus page/component

---
+++ client/app/components/aboutus/aboutus.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aboutus',
+  templateUrl: './client/app/components/aboutus/aboutus.html',
+  styleUrls: ['./client/app/components/aboutus/aboutus.css'],
+})
+
+export class AboutUsComponent {
+}
